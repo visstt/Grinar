6b0d04e9c0e864ba7552f878f1ad856cbdb1dd1f
visstt <egorskomorohov020606@gmail.com>
2025-07-05 00:20:51 +0500

    fix: apdate home page component for better mobile adaptive

--- src/shared/ui/footer/Footer.jsx
+++ src/shared/ui/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import styles from "./Footer.module.css";
 
 export default function Footer() {
@@ -6,13 +7,10 @@ export default function Footer() {
     <div className={styles.footer}>
       <div className="container">
         <div className={styles.footer_wrapper}>
-          <p>Идея и разработка: grinar_studio</p>
-          <div className={styles.group}>
-            <p>Оферта</p>
-            <p>Правила пользования</p>
-            <p>Политика конфиденциальности</p>
-            <p>Юридическая информация</p>
-          </div>
+          <p>Оферта</p>
+          <p>Правила пользования</p>
+          <p>Политика конфиденциальности</p>
+          <p>Юридическая информация</p>
         </div>
       </div>
     </div>
