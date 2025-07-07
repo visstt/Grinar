be3ac7a7d8917e0afa17301174c3381f0e1aabfb
visstt <egorskomorohov020606@gmail.com>
2025-07-07 13:54:36 +0500

    feat: Refactor ProfilePage component and introduce ProfileTitle for user details display

--- src/pages/profilePage/ProfilePage.jsx
+++ src/pages/profilePage/ProfilePage.jsx
@@ -1,48 +1,9 @@
-import Header from "../../shared/ui/components/header/Header";
-import styles from "./ProfilePage.module.css";
+import ProfileTitle from "./profileComponents/profileTitle/ProfileTitle";
 
 export default function ProfilePage() {
   return (
-    <div className={styles.background}>
-      <Header />
-      <div className="container">
-        <div className={styles.wrapper}>
-          <div className={styles.user_info}>
-            <h1>Алексеев Александр</h1>
-            <div className={styles.user_info__details}>
-              <p>SMM</p>
-              <p>Москва</p>
-            </div>
-          </div>
-          <div className={styles.user_logo}>
-            <img src="/icons/userLogo.png" alt="userLogo" />
-          </div>
-
-          <div className={styles.user_stats}>
-            <div className={styles.stats}>
-              <div className={styles.inf} style={{ marginRight: "30px" }}>
-                <img src="/public/icons/star.svg" alt="star" />
-                <p>1 296</p>
-              </div>
-              <div className={styles.inf}>
-                <img src="/public/icons/like.svg" alt="like" />
-                <p>138</p>
-              </div>
-              <div className={styles.inf}>
-                <img src="/public/icons/profile-2user.svg" alt="user" />
-                <p>24</p>
-              </div>
-            </div>
-
-            <div className={styles.settings}>
-              <button className={styles.settingsBtn}>
-                <img src="/icons/setting.svg" alt="settings" />
-                <p>Настройки</p>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <ProfileTitle />
+    </>
   );
 }
---
+++ src/pages/profilePage/profileComponents/profileTitle/ProfileTitle.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import Header from "../../../../shared/ui/components/header/Header";
+import styles from "./ProfileTtile.module.css";
+
+export default function ProfileTitle() {
+  return (
+    <div className={styles.background}>
+      <Header />
+      <div className="container">
+        <div className={styles.wrapper}>
+          <div className={styles.user_info}>
+            <h1>Алексеев Александр</h1>
+            <div className={styles.user_info__details}>
+              <p>SMM</p>
+              <p>Москва</p>
+            </div>
+          </div>
+          <div className={styles.user_logo}>
+            <img src="/icons/userLogo.png" alt="userLogo" />
+          </div>
+
+          <div className={styles.user_stats}>
+            <div className={styles.stats}>
+              <div className={styles.inf}>
+                <img src="/icons/star.svg" alt="star" />
+                <p>1 296</p>
+              </div>
+              <div className={styles.inf}>
+                <img src="/icons/like.svg" alt="like" />
+                <p>138</p>
+              </div>
+              <div className={styles.inf}>
+                <img src="/icons/profile-2user.svg" alt="user" />
+                <p>24</p>
+              </div>
+            </div>
+
+            <div className={styles.settings}>
+              <button className={styles.settingsBtn}>
+                <img src="/icons/setting.svg" alt="settings" />
+                <p>Настройки</p>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
