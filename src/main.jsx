fff6cbc80e718a96ee984c4e6c0f49cfa26c40e1
visstt <egorskomorohov020606@gmail.com>
2025-08-15 22:48:45 +0500

    refactor: Remove DevPasswordGate wrapper from main application render

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
 
-import DevPasswordGate from "../src/shared/DevPasswordGate.jsx";
 import App from "./app/App.jsx";
 import "./app/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <DevPasswordGate>
-      <App />
-    </DevPasswordGate>
+    <App />
   </StrictMode>,
 );
